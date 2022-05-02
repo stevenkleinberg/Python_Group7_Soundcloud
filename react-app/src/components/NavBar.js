0ba8818fa4798b4d0d80e577aad128fc6e433bd0
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-05-02 15:33:59 -0700

    dev links for upload and edit

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,30 +1,39 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
 
 const NavBar = () => {
   return (
     <nav>
       <ul>
         <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
+          <NavLink to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
+          <NavLink to="/login" exact={true} activeClassName="active">
             Login
           </NavLink>
         </li>
         <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+          <NavLink to="/sign-up" exact={true} activeClassName="active">
             Sign Up
           </NavLink>
         </li>
         <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
+          <NavLink to="/users" exact={true} activeClassName="active">
             Users
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/upload-song" exact={true} activeClassName="active">
+            Upload
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/edit-song" exact={true} activeClassName="active">
+            Edit
           </NavLink>
         </li>
         <li>
@@ -33,6 +42,6 @@ const NavBar = () => {
       </ul>
     </nav>
   );
-}
+};
 
 export default NavBar;
