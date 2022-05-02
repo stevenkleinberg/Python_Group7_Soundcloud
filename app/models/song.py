0d4c4d0f0b4ecb9a59e89aa6850d783d65c2140f
leonphoenix21 <scamonoel169@gmail.com>
2022-05-02 15:12:50 -0500

    likes join tables

--- app/models/song.py
+++ app/models/song.py
@@ -1,31 +1,37 @@
 from .db import db
 from datetime import datetime
+from .song_like import song_likes
 
 
 class Song(db.Model):
     __tablename__ = 'songs'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     title = db.Column(db.String(75), nullable=False)
-    audio_url = db.Column(db.String, nullable=False )
+    audio_url = db.Column(db.String, nullable=False)
     description = db.Column(db.Text)
     image_url = db.Column(db.String)
-    created_at = db.Column(db.DateTime, nullable=False,default=datetime.now())
-    updated_at = db.Column(db.DateTime, nullable=False,default=datetime.now())
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
 
-    user = db.relationship("User",back_populates="songs")
-    comments =  db.relationship("Comment", back_populates="song")
+    user = db.relationship("User", back_populates="songs")
+    comments = db.relationship("Comment", back_populates="song")
 
+    likes = db.relationship(
+        "User",
+        secondary=song_likes,
+        back_populates="likes"
+    )
 
     def to_dict(self):
         return {
             'id': self.id,
             'user_id': self.user_id,
             'title': self.title,
-            'audio_url':self.audio_url,
-            'description' : self.description,
-            'image_url':self.image_url,
+            'audio_url': self.audio_url,
+            'description': self.description,
+            'image_url': self.image_url,
             'created_at': self.created_at,
             'updated_at': self.updated_at
         }
