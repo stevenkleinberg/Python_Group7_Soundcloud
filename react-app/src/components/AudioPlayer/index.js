acb6da8d8d349816197d1c3e434e4a384ffc634c
adamtang5 <adamtang@gmail.com>
2022-05-02 23:28:03 -0700

    copied component from Edwin's repo

---
+++ react-app/src/components/AudioPlayer/index.js
@@ -0,0 +1,22 @@
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css';
+import { useSelector } from 'react-redux';
+import LoginFormModal from '../LoginFormModal';
+import { NavLink } from 'react-router-dom';
+import './index.css'
+
+const Audio = () => {
+    const song = useSelector(state => state.song.playingSong)
+    const sessionUser = useSelector(state => state.session.user)
+    return (
+        <div className="player">
+            <div className='songinfo'>
+                <img src={song.imageUrl} onError={(e) => e.target.src = require('../../files/default.png')} height='50px' />
+                {sessionUser ? <NavLink to={`/songs/${song.id}`}>{song.title}</NavLink> : <LoginFormModal name={song.title} />}
+            </div>
+            <AudioPlayer customAdditionalControls={[]} layout="horizontal-reverse" src={song.url} />
+        </div>
+    )
+}
+
+export default Audio
