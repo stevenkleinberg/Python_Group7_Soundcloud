12c6ad103952a94fe8d56fcccc7b791ed0a84033
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-05-05 14:25:42 -0700

    reamoving head and main thing

--- react-app/src/components/SongFolders/SingleSong/index.js
+++ react-app/src/components/SongFolders/SingleSong/index.js
@@ -1,81 +1,55 @@
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-<<<<<<< HEAD
+import SongComments from "./songComments";
 import "./singlesong.css";
-
+import Moment from "react-moment";
 const SingleSong = () => {
   const { id } = useParams();
-  const songs = useSelector((state) => state.songs);
-  const song = songs[+id];
+  const song = useSelector((state) => state.songs[id]);
 
+  // return (
+  //     <div>
+  //         <img
+  //             id='songImg'
+  //             src={song?.image_url}
+  //             alt=' '
+  //             style={{ width: '100%' }}
+  //         />
+  //         {song?.title}
+
+  //         <NavLink to={`/songs/${+id}/edit`} exact={true} activeClassName="active">
+  //             edit form
+  //         </NavLink>
+  //     </div>
+  // )
   return (
-    <div>
-      <img
-        id="songImg"
-        src={song?.image_url}
-        alt=" "
-        style={{ width: "100%" }}
-      />
-      {song?.title}
-
-      <NavLink to={`/songs/${+id}/edit`} exact={true} activeClassName="active">
-        edit form
-      </NavLink>
+    <div className="song_container_main">
+      <div className="Pl_S_banner flex-row">
+        <div className="left_box_banner flex-column">
+          <div className="title_banner flex-row">
+            <div className="flex-row banner_title_group_1">
+              <div className="banner_play_button">
+                <p>Play</p>
+              </div>
+              <div className="flex-column">
+                <h3>{song?.title}</h3>
+                <p>{song?.description}</p>
+              </div>
+            </div>
+            <Moment fromNow>{song?.created_at}</Moment>
+          </div>
+        </div>
+        <div>
+          <img src={song?.image_url} className="song_image" />
+        </div>
+      </div>
+      <div className="flex-row song_mainfeed_sidebar_conatiner">
+        <SongComments song={song} />
+        {/* <songSideBar /> */}
+      </div>
     </div>
   );
 };
-=======
-import SongComments from './songComments';
-import './singlesong.css'
-import Moment from "react-moment";
-const SingleSong = () => {
-    const { id } = useParams();
-    const song = useSelector(state => state.songs[id])
-
-    // return (
-    //     <div>
-    //         <img
-    //             id='songImg'
-    //             src={song?.image_url}
-    //             alt=' '
-    //             style={{ width: '100%' }}
-    //         />
-    //         {song?.title}
-
-    //         <NavLink to={`/songs/${+id}/edit`} exact={true} activeClassName="active">
-    //             edit form
-    //         </NavLink>
-    //     </div>
-    // )
-    return (
-        <div className="song_container_main">
-          <div className="Pl_S_banner flex-row">
-            <div className="left_box_banner flex-column">
-              <div className="title_banner flex-row">
-                <div className="flex-row banner_title_group_1">
-                  <div className="banner_play_button">
-                    <p>Play</p>
-                  </div>
-                  <div className="flex-column">
-                    <h3>{song?.title}</h3>
-                    <p>{song?.description}</p>
-                  </div>
-                </div>
-                <Moment fromNow>{song?.created_at}</Moment>
-              </div>
-            </div>
-            <div>
-              <img src={song?.image_url} className="song_image" />
-            </div>
-          </div>
-          <div className="flex-row song_mainfeed_sidebar_conatiner">
-            <SongComments song={song} />
-            {/* <songSideBar /> */}
-          </div>
-        </div>
-      );
-}
->>>>>>> main
 
 export default SingleSong;
