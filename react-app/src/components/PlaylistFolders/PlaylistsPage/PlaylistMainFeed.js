ec60b09e2b2dfbd4d933d10820c0a90ec37b6e71
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-05-04 19:51:23 -0700

    started on playlistmainfeed.js

---
+++ react-app/src/components/PlaylistFolders/PlaylistsPage/PlaylistMainFeed.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PlaylistMainFeed = (songsId) => {
+  return (
+    <div>
+      <div>
+        <button>Like</button>
+        <button>Share</button>
+        <button>Copy Link</button>
+        <button>Edit</button>
+        <button>More</button>
+      </div>
+    </div>
+  );
+};
+
+export default PlaylistMainFeed;
--- react-app/src/components/PlaylistFolders/PlaylistsPage/index.js
+++ react-app/src/components/PlaylistFolders/PlaylistsPage/index.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import Moment from "react-moment";
 
 import NewPlaylistForm from "../CreatePlaylist";
+import PlaylistMainFeed from "./PlaylistMainFeed";
 
 const PlaylistsPage = () => {
   const { id } = useParams();
@@ -16,30 +17,33 @@ const PlaylistsPage = () => {
   console.log(todayDate.getDay() - createdDate.getDay());
 
   return (
-    <div className="Pl_S_banner flex-row">
-      <div className="left_box_banner flex-column">
-        <div className="title_banner flex-row">
-          <div className="flex-row banner_title_group_1">
-            <div className="banner_play_button">
-              <p>Play</p>
+    <div>
+      <div className="Pl_S_banner flex-row">
+        <div className="left_box_banner flex-column">
+          <div className="title_banner flex-row">
+            <div className="flex-row banner_title_group_1">
+              <div className="banner_play_button">
+                <p>Play</p>
+              </div>
+              <div className="flex-column">
+                <h3>{playlist?.title}</h3>
+                <p>{playlist?.description}</p>
+              </div>
             </div>
-            <div className="flex-column">
-              <h3>{playlist?.title}</h3>
-              <p>{playlist?.description}</p>
-            </div>
+            <Moment fromNow>{playlist?.created_at}</Moment>
           </div>
-          <Moment fromNow>{playlist?.created_at}</Moment>
+          <div className="banner_circle">
+            <div>{playlist?.songs.length}</div>
+            <div>tracks</div>
+          </div>
         </div>
-        <div className="banner_circle">
-          <div>{playlist?.songs.length}</div>
-          <div>tracks</div>
+        <div>
+          <img src={playlist?.image_url} className="playlist_image" />
         </div>
       </div>
       <div>
-        <img src={playlist?.image_url} className="playlist_image" />
+        <PlaylistMainFeed songsId={playlist?.songs} />
       </div>
-      {/* <h1>Playlist Page</h1>
-      <NewPlaylistForm /> */}
     </div>
   );
 };
