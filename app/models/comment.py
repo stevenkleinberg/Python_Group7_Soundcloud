ecaf88fb84324f2348a1500f33a8329974bacf26
adamtang5 <adamtang@gmail.com>
2022-05-05 12:45:50 -0700

    inspected comments model file

--- app/models/comment.py
+++ app/models/comment.py
@@ -6,13 +6,13 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"),nullable=False)
-    song_id = db.Column(db.Integer,db.ForeignKey("songs.id"),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    song_id = db.Column(db.Integer, db.ForeignKey("songs.id"), nullable=False)
     song_timestamp = db.Column(db.Time)
-    created_at = db.Column(db.DateTime, nullable=False,default=datetime.now())
-    updated_at = db.Column(db.DateTime, nullable=False,default=datetime.now())
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
 
-    user = db.relationship("User",back_populates="comments")
+    user = db.relationship("User", back_populates="comments")
     song = db.relationship("Song", back_populates="comments")
 
     def to_dict(self):
