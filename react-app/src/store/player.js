f7a233298aa226477cf1bf431539bdc0cbfbc121
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-05-05 09:23:37 -0700

    changed playlistid to songs_order in players.js thunk

--- react-app/src/store/player.js
+++ react-app/src/store/player.js
@@ -1,41 +1,42 @@
 // constants
-const LOAD_SONG = 'player/LOAD_SONG';
-const LOAD_PLAYLIST = 'player/LOAD_PLAYLIST';
-const QUEUE_SONG = 'player/QUEUE_SONG';
-const QUEUE_PLAYLIST = 'player/QUEUE_PLAYLIST';
-const QUEUE_ADVANCE = 'player/QUEUE_ADVANCE';
-const HISTORY_STEPBACK = 'player/HISTORY_STEPBACK';
+const LOAD_SONG = "player/LOAD_SONG";
+const LOAD_PLAYLIST = "player/LOAD_PLAYLIST";
+const QUEUE_SONG = "player/QUEUE_SONG";
+const QUEUE_PLAYLIST = "player/QUEUE_PLAYLIST";
+const QUEUE_ADVANCE = "player/QUEUE_ADVANCE";
+const HISTORY_STEPBACK = "player/HISTORY_STEPBACK";
 
 // adding song to play now and history
 export const loadSong = (songId) => ({
-    type: LOAD_SONG,
-    songId,
+  type: LOAD_SONG,
+  songId,
 });
 
-export const loadPlaylist = (playlistId) => ({
-    type: LOAD_PLAYLIST,
-    playlistId,
+// playlist.songs_order
+export const loadPlaylist = (songs_order) => ({
+  type: LOAD_PLAYLIST,
+  songs_order,
 });
 
 // adding song to future queue
 export const queueSong = (songId) => ({
-    type: QUEUE_SONG,
-    songId,
+  type: QUEUE_SONG,
+  songId,
 });
 
-export const queuePlaylist = (playlistId) => ({
-    type: QUEUE_PLAYLIST,
-    playlistId,
+export const queuePlaylist = (songs_order) => ({
+  type: QUEUE_PLAYLIST,
+  songs_order,
 });
 
 // moving song from future queue into now playing and history
 export const queueAdvance = () => ({
-    type: QUEUE_ADVANCE,
+  type: QUEUE_ADVANCE,
 });
 
 // moving song from history into now playing and move now playing into queue
 export const historyStepBack = () => ({
-    type: HISTORY_STEPBACK,
+  type: HISTORY_STEPBACK,
 });
 
 // State shape:
@@ -47,59 +48,57 @@ export const historyStepBack = () => ({
 // }
 
 const initialState = {
-    playingId: null,
-    playHistory: [],
-    queue: [],
+  playingId: null,
+  playHistory: [],
+  queue: [],
 };
 
 export default function reducer(state = initialState, action) {
-    switch (action.type) {
-        case LOAD_SONG: {
-            const newState = {
-                ...state,
-            };
-            if (newState.playingId) {
-                newState.playHistory = [...newState.playHistory, newState.playingId];
-            }
-            newState.playingId = action.songId;
-            return newState;
-        }
-        case QUEUE_SONG: {
-            const newState = {
-                ...state,
-            };
-            newState.queue = [
-                ...newState.queue, action.songId,
-            ];
-            return newState;
-        }
-        case QUEUE_ADVANCE: {
-            const newState = { ...state };
-            if (newState.playingId) {
-                newState.playHistory = [...newState.playHistory, newState.playingId];
-            }
-            if (newState.queue.length) {
-                newState.playingId = newState.queue[0];
-                newState.queue = newState.queue.slice(1);
-            } else {
-                newState.playingId = null;
-            }
-            return newState;
-        }
-        case HISTORY_STEPBACK: {
-            const newState = { ...state };
-            if (newState.playingId) {
-                newState.queue = [newState.playingId, ...newState.queue];
-            }
-            if (newState.playHistory.length) {
-                newState.playingId = newState.playHistory.pop();
-                newState.playHistory = [...newState.playHistory];
-            } else {
-                newState.playingId = null;
-            }
-            return newState;
-        }
-        default:
-            return state;
+  switch (action.type) {
+    case LOAD_SONG: {
+      const newState = {
+        ...state,
+      };
+      if (newState.playingId) {
+        newState.playHistory = [...newState.playHistory, newState.playingId];
+      }
+      newState.playingId = action.songId;
+      return newState;
     }
+    case QUEUE_SONG: {
+      const newState = {
+        ...state,
+      };
+      newState.queue = [...newState.queue, action.songId];
+      return newState;
+    }
+    case QUEUE_ADVANCE: {
+      const newState = { ...state };
+      if (newState.playingId) {
+        newState.playHistory = [...newState.playHistory, newState.playingId];
+      }
+      if (newState.queue.length) {
+        newState.playingId = newState.queue[0];
+        newState.queue = newState.queue.slice(1);
+      } else {
+        newState.playingId = null;
+      }
+      return newState;
+    }
+    case HISTORY_STEPBACK: {
+      const newState = { ...state };
+      if (newState.playingId) {
+        newState.queue = [newState.playingId, ...newState.queue];
+      }
+      if (newState.playHistory.length) {
+        newState.playingId = newState.playHistory.pop();
+        newState.playHistory = [...newState.playHistory];
+      } else {
+        newState.playingId = null;
+      }
+      return newState;
+    }
+    default:
+      return state;
+  }
 }
