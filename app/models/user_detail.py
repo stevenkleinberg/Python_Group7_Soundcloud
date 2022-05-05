5cf2f7abd1aa48cb39745008cbc292af183fc4ae
Steven Kleinberg <stevenkleinberg5@gmail.com>
2022-05-05 12:00:38 -0500

    userdetails migration

--- app/models/user_detail.py
+++ app/models/user_detail.py
@@ -2,7 +2,7 @@ from .db import db
 
 
 class UserDetail(db.Model):
-    __tablename__ = "userDetail"
+    __tablename__ = "userDetails"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     display_name = db.Column(db.String(50))
