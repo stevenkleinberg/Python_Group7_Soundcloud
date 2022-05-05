982db3db7d8a74b76acbf12cc3726d4277827788
Steven Kleinberg <stevenkleinberg5@gmail.com>
2022-05-05 12:32:36 -0500

    fixed it

--- app/seeds/user_details.py
+++ app/seeds/user_details.py
@@ -4,24 +4,21 @@ from app.models import db, UserDetail
 # Adds a demo user, you can add other users here if you want
 def seed_user_details():
     demo = UserDetail(
-        id=1,
-        user_id='1',
+        user_id=1,
         display_name='Demo user'
     )
     marnie = UserDetail(
-        id=2,
         user_id=2,
         display_name='Marnie Bernard'
     )
     bobbie = UserDetail(
-        id=3,
         user_id=3,
         display_name='Bobby Bernard'
     )
 
-    db.session.add(dict(demo))
-    db.session.add(dict(marnie))
-    db.session.add(dict(bobbie))
+    db.session.add(demo)
+    db.session.add(marnie)
+    db.session.add(bobbie)
 
     db.session.commit()
 
@@ -32,5 +29,5 @@ def seed_user_details():
 # resets the auto incrementing primary key, CASCADE deletes any
 # dependent entities
 def undo_user_details():
-    db.session.execute('TRUNCATE userDetails RESTART IDENTITY CASCADE;')
+    db.session.execute('TRUNCATE "userDetails" RESTART IDENTITY CASCADE;')
     db.session.commit()
