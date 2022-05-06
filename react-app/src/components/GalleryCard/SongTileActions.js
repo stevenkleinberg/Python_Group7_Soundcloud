639c1e4694961a44f09337963973d9e67d54323b
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-05-06 10:17:19 -0700

    new branch

--- react-app/src/components/GalleryCard/SongTileActions.js
+++ react-app/src/components/GalleryCard/SongTileActions.js
@@ -1,53 +1,70 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { loadSong, queueSong } from '../../store/player';
-
+import { Modal } from "../Context/Modal";
+import { loadSong, queueSong } from "../../store/player";
+import AddtoPlaylist from "../PlaylistFolders/AddtoPlaylist";
 
 function SongTileActions({ song }) {
-    const playingId = useSelector(state => state.player.playingId);
-    const dispatch = useDispatch();
-    const [showMenu, setShowMenu] = useState(false);
+  const playingId = useSelector((state) => state.player.playingId);
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const [showPlaylistModal, setShowPlaylistModal] = useState(false);
 
-    const openMenu = () => {
-        if (showMenu) return;
-        setShowMenu(true);
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  const openPlaylistModal = () => {
+    if (showPlaylistModal) return;
+    setShowPlaylistModal(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
     };
 
-    useEffect(() => {
-        if (!showMenu) return;
+    document.addEventListener("click", closeMenu);
 
-        const closeMenu = () => {
-            setShowMenu(false);
-        };
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
-        document.addEventListener('click', closeMenu);
-
-        return () => document.removeEventListener("click", closeMenu);
-    }, [showMenu]);
-
-    const addSongToQueue = (id) => {
-        if (!playingId) {
-            dispatch(loadSong(id));
-        } else {
-            dispatch(queueSong(id));
-        }
+  const addSongToQueue = (id) => {
+    if (!playingId) {
+      dispatch(loadSong(id));
+    } else {
+      dispatch(queueSong(id));
     }
+  };
 
-    return (
-        <>
-            <div>
-                <div className="song_tile_actions" onClick={openMenu}>...</div>
-                <div className="dropdown">
-                    {showMenu && (
-                        <div className="song-tile-action-dropdown">
-                            <div onClick={() => addSongToQueue(song.id)}>Add to Queue</div>
-                        </div>
-                    )}
-                </div>
+  return (
+    <>
+      <div>
+        <div className="song_tile_actions" onClick={openMenu}>
+          ...
+        </div>
+        <div className="dropdown">
+          {showMenu && (
+            <div className="song-tile-action-dropdown">
+              <div onClick={() => addSongToQueue(song.id)}>Add to Queue</div>
+              <div onClick={openPlaylistModal}>Add to Playlist</div>
             </div>
-        </>
-    );
+          )}
+        </div>
+        {showPlaylistModal && (
+          <Modal onClose={() => setShowPlaylistModal(false)}>
+            <div className="add_to_playlist_modal_container">
+              <AddtoPlaylist />
+            </div>
+          </Modal>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default SongTileActions;
---
+++ react-app/src/components/PlaylistFolders/AddtoPlaylist/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+
+import "./AddtoPlaylist.css";
+
+const AddtoPlaylist = () => {
+  const [filter, setFilter] = useState("");
+  const user = useSelector((state) => state.session.user);
+  const playlistsObj = useSelector((state) => state.playlists);
+  const raw_playlists = Object.values(playlistsObj);
+  const playlists = raw_playlists.filter(
+    (playlist) => playlist.user_id === user.id
+  );
+
+  return (
+    <div className="flex-column inner_add_to_playlist_conatiner">
+      <div className="flex-row">
+        <p>Add to playlist</p>
+        <p>Create a playlist</p>
+      </div>
+      <input
+        value={filter}
+        placeholder="Filter playlist"
+        onChange={(e) => setFilter(e.target.value.toLowerCase())}
+      />
+      <div className="div_row_add_to_playlist_container">
+        {playlists
+          .filter((playlist) => playlist?.title.toLowerCase().includes(filter))
+          .map((playlist) => (
+            <div
+              key={playlist?.id}
+              className="flex-row div_row_add_to_playlist"
+            >
+              <div className="flex-row">
+                <img
+                  src={playlist?.image_url}
+                  className="add_to_playlist_img"
+                />
+                <div>
+                  <p>{playlist?.title}</p>
+                  <p>{playlist?.songs.length}</p>
+                </div>
+              </div>
+              <button>Add to playlist</button>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default AddtoPlaylist;
