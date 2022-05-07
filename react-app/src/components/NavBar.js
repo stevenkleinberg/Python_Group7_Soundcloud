a5db9e38e03f092a52042e26ab4054cdf477c54c
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-05-07 00:32:05 -0700

    searchbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import LogoutButton from "./auth/LogoutButton";
 import Logo from "./Icons/Logo";
 import "./NavBar.css";
 import UserProfile from "./UserProfile";
 
 const NavBar = () => {
   const sessionUser = useSelector((state) => state.session.user);
+  const rawSongs = useSelector((state) => state.songs);
+  const songs = Object.values(rawSongs);
+
+  console.log(songs);
+
+  const [showResults, setShowResults] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    if (searchInput.length) {
+      setShowResults(true);
+    } else {
+      setShowResults(false);
+    }
+  }, [searchInput]);
+
+  useEffect(() => {
+    if (!showResults) return;
+
+    const closeResults = () => {
+      setShowResults(false);
+    };
+
+    document.addEventListener("click", closeResults);
+
+    return () => document.removeEventListener("click", closeResults);
+  }, [showResults]);
+
   let sessionLinks = (
     <nav className="navbar">
       <NavLink
@@ -19,13 +46,12 @@ const NavBar = () => {
         <Logo />
       </NavLink>
       <NavLink
-        className="navlinks"
-        to="/upload-song"
+        className="navlinks logo"
+        to="/"
         exact={true}
         activeClassName="active"
       >
-        {" "}
-        Upload{" "}
+        Home
       </NavLink>
       <NavLink
         className="navlinks"
@@ -35,6 +61,50 @@ const NavBar = () => {
       >
         {" "}
         Library{" "}
+      </NavLink>
+      <div className="flex-row navbar_input_container">
+        <div>
+          <input
+            className="navbar_search"
+            placeholder="Search"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          {showResults && (
+            <div className="search_results">
+              <ul>
+                {songs
+                  ?.filter((song) =>
+                    song.title.toLowerCase().includes(searchInput.toLowerCase())
+                  )
+                  .map((song) => (
+                    <li key={song.id} className="flex-row search_results_li">
+                      <img
+                        className="search_results_img"
+                        src={song.image_url}
+                      />
+                      <a
+                        href={`/songs/${song.id}`}
+                        className="search_results_a"
+                      >
+                        {song.title}
+                      </a>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <NavLink
+        className="navlinks"
+        to="/upload-song"
+        exact={true}
+        activeClassName="active"
+      >
+        {" "}
+        Upload{" "}
       </NavLink>
       <UserProfile user={sessionUser} />
     </nav>
