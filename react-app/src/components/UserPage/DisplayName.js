74f791c583db8497da70951db3cabbb5c8da7130
leonphoenix21 <scamonoel169@gmail.com>
2022-05-09 21:39:36 -0500

    Dried up the userPage Code

--- react-app/src/components/UserPage/DisplayName.js
+++ react-app/src/components/UserPage/DisplayName.js
@@ -10,31 +10,14 @@ function DisplayName() {
     const { userId } = useParams();
     const sessionUser = useSelector((state) => state.session.user);
     const currentDetails = useSelector((state) => state.details[userId]);
-    const songs = Object.values(useSelector((state) => state.songs));
 
-    const likedSongs = songs?.filter((song) => song.likes.includes(+userId));
 
     const dispatch = useDispatch();
-    const history = useHistory();
     const [display_name, setDisplayName] = useState(currentDetails?.display_name);
     const [avatar_url, setUrl] = useState("");
     const [banner_url, setBannerUrl] = useState(currentDetails?.banner_url);
     const [activity, setActivity] = useState(false);
     const [display_box, setDisplayBox] = useState(false);
-
-    // const handleSubmit = async (ev) => {
-    //     ev.preventDefault();
-
-    //     const formData = new FormData();
-    //     formData.append("id", currentDetails.id);
-    //     formData.append("avatar_url", avatar_url);
-    //     formData.append("display_name", display_name);
-    //     formData.append("banner_url", banner_url);
-
-    //     const detail = await dispatch(editDetails(formData));
-
-    //     setDisplayBox(false);
-    // };
 
     const handleSubmit = async (ev) => {
         ev.preventDefault();
@@ -51,20 +34,6 @@ function DisplayName() {
         setDisplayBox(false);
     };
 
-    const verify = currentDetails?.id === +sessionUser.id;
-
-    const updateAvatarUrl = (e) => {
-        const file = e.target.files[0];
-        setUrl(file);
-    };
-
-    const updateActivity = (e) => {
-        if (e.target.files) {
-            setActivity(true);
-        } else {
-            setActivity(false);
-        }
-    };
     const updateSetDisplayBox = () => {
         if (sessionUser.id === currentDetails.id) {
             setDisplayBox(true)
@@ -89,10 +58,7 @@ function DisplayName() {
             setDisplayBox(false);
         }
     };
-    const updateBannerUrl = (e) => {
-        const file = e.target.files[0];
-        setBannerUrl(file);
-    };
+
     return (
         <div className="flex-column">
             {display_box ? (
--- react-app/src/components/UserPage/index.js
+++ react-app/src/components/UserPage/index.js
@@ -13,76 +13,9 @@ import "./user_page.scss";
 
 function UserPage() {
   const { userId } = useParams();
-  const sessionUser = useSelector((state) => state.session.user);
-  const currentDetails = useSelector((state) => state.details[userId]);
   const songs = Object.values(useSelector((state) => state.songs));
 
   const likedSongs = songs?.filter((song) => song.likes.includes(+userId));
-
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const [display_name, setDisplayName] = useState("");
-  const [avatar_url, setUrl] = useState("");
-  const [banner_url, setBannerUrl] = useState("");
-  const [activity, setActivity] = useState(false);
-  const [display_box, setDisplayBox] = useState(false);
-
-  const handleSubmit = async (ev) => {
-    ev.preventDefault();
-
-    const formData = new FormData();
-    formData.append("id", currentDetails.id);
-    formData.append("avatar_url", avatar_url);
-    formData.append("display_name", display_name);
-    formData.append("banner_url", banner_url);
-
-    const detail = await dispatch(editDetails(formData));
-
-    setDisplayBox(false);
-  };
-
-  const verify = currentDetails?.id === +sessionUser.id;
-
-  const updateAvatarUrl = (e) => {
-    const file = e.target.files[0];
-    setUrl(file);
-  };
-
-  const updateActivity = (e) => {
-    if (e.target.files) {
-      setActivity(true);
-    } else {
-      setActivity(false);
-    }
-  };
-  const updateSetDisplayBox = () => {
-    if (sessionUser.id === currentDetails.id) {
-      setDisplayBox(true)
-    } else {
-      setDisplayBox(false)
-    }
-  }
-
-  const updateActivityDisplay = (e) => {
-    if (e === null) setActivity(false)
-    if (e.target.value) {
-      setActivity(true);
-    } else {
-      setActivity(false);
-    }
-  };
-
-  const checkDisplayName = (e) => {
-    if (e) {
-      setDisplayBox(true);
-    } else {
-      setDisplayBox(false);
-    }
-  };
-  const updateBannerUrl = (e) => {
-    const file = e.target.files[0];
-    setBannerUrl(file);
-  };
 
   return (
     <>
