b73083399c0e6077ea3f3550edff1b960bd83186
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-06-08 16:51:35 -0700

    playlist page

--- react-app/src/components/NewUserPage/usersplaylists.js
+++ react-app/src/components/NewUserPage/usersplaylists.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import PlaylistTile from "../GalleryCard/playlistTile";
 
 const UsersPlaylists = () => {
-  return <div></div>;
+  const { userId } = useParams();
+  const allPlaylists = useSelector((state) => state.playlists);
+  const allPlaylistsArr = Object.values(allPlaylists);
+
+  const userPlaylists = allPlaylistsArr.filter(
+    (playlist) => +playlist?.user_id === +userId
+  );
+  return (
+    <div className="playlist_tile_container flex-row">
+      {userPlaylists.map((playlist) => (
+        <PlaylistTile key={playlist.id} playlist={playlist} />
+      ))}
+    </div>
+  );
 };
 
 export default UsersPlaylists;
