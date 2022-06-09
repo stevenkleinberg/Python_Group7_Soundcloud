23a57fd438eb87d254216761d9d23776ae305e20
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-06-09 14:37:33 -0700

    trst

--- react-app/src/components/AudioPlayer/QueueBox.js
+++ react-app/src/components/AudioPlayer/QueueBox.js
@@ -4,9 +4,14 @@ import SingleSong from "./SingleSong";
 
 const QueueBox = () => {
   const queue = useSelector((state) => state.player.queue);
+  const playHistory = useSelector((state) => state.player.playHistory);
+  const currSong = useSelector((state) => state.player.playingId);
+
+  const listArr = [...playHistory, currSong, ...queue];
+  console.log("🚀 ~ file: QueueBox.js ~ line 11 ~ QueueBox ~ listArr", listArr);
   return (
     <div className="queue_box_container">
-      {queue?.map((songId, idx) => (
+      {listArr?.map((songId, idx) => (
         <SingleSong key={idx} songId={songId} />
       ))}
     </div>
