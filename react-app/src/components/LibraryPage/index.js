fcabcbce33e52b1c80503aaffcd57e7cc21815da
adamtang5 <adamtang@gmail.com>
2022-06-08 21:56:03 -0700

    styled library page

--- react-app/src/components/LibraryPage/index.js
+++ react-app/src/components/LibraryPage/index.js
@@ -11,56 +11,58 @@ const LibraryPage = () => {
   const [selected, setSelected] = useState("songs");
 
   return (
-    <div className="library_container flex-column">
-      <div className="inner_library_container flex-column">
-        <div className="flex-row library_links_container">
-          <NavLink
-            to="/library/songs"
-            className={
-              selected === "songs"
-                ? `library_links library_links_selected`
-                : `library_links`
-            }
-            onClick={() => setSelected("songs")}
-          >
-            My Songs
-          </NavLink>
-          <NavLink
-            to="/library/likes"
-            className={
-              selected === "likes"
-                ? `library_links library_links_selected`
-                : `library_links`
-            }
-            onClick={() => setSelected("likes")}
-          >
-            Likes
-          </NavLink>
-          <NavLink
-            to="/library/playlists"
-            className={
-              selected === "playlists"
-                ? `library_links library_links_selected`
-                : `library_links`
-            }
-            onClick={() => setSelected("playlists")}
-          >
-            Playlist
-          </NavLink>
-        </div>
-        <Switch>
-          <ProtectedRoute path={"/library/songs"} exact={true}>
-            <SongsList />
-          </ProtectedRoute>
-          <ProtectedRoute path={"/library/likes"} exact={true}>
-            <Likes />
-          </ProtectedRoute>
-          <ProtectedRoute path={"/library/playlists"} exact={true}>
-            <Playlist />
-          </ProtectedRoute>
-        </Switch>
+    <>
+      <div className="library_links_container flex-row">
+        <NavLink
+          to="/library/songs"
+          className={
+            selected === "songs"
+              ? `library_links library_links_selected`
+              : `library_links`
+          }
+          onClick={() => setSelected("songs")}
+        >
+          My Songs
+        </NavLink>
+        <NavLink
+          to="/library/likes"
+          className={
+            selected === "likes"
+              ? `library_links library_links_selected`
+              : `library_links`
+          }
+          onClick={() => setSelected("likes")}
+        >
+          Likes
+        </NavLink>
+        <NavLink
+          to="/library/playlists"
+          className={
+            selected === "playlists"
+              ? `library_links library_links_selected`
+              : `library_links`
+          }
+          onClick={() => setSelected("playlists")}
+        >
+          Playlist
+        </NavLink>
       </div>
-    </div>
+      <div className="library_container flex-column">
+        <div className="inner_library_container flex-column">
+          <Switch>
+            <ProtectedRoute path={"/library/songs"} exact={true}>
+              <SongsList />
+            </ProtectedRoute>
+            <ProtectedRoute path={"/library/likes"} exact={true}>
+              <Likes />
+            </ProtectedRoute>
+            <ProtectedRoute path={"/library/playlists"} exact={true}>
+              <Playlist />
+            </ProtectedRoute>
+          </Switch>
+        </div>
+      </div>
+    </>
   );
 };
 
